fe23525de9eb009f1965e2716f61d8ef5285373a
Manoj Bhargavan <manoj.bhargavan@outlook.in>
2023-06-19 16:45:03 +0530

    feature txt changed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed onm feature branch
+          Learn React Nandu Bass
         </a>
       </header>
     </div>
